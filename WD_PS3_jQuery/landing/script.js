e3b7bcf29bbc879e0cd8b22cf9719d2895774125
IgorPopov <ginger_tulips@protonmail.com>
2018-11-24 12:08:24 +0200

    Changes on the first review

--- WD_PS3_jQuery/landing/script.js
+++ WD_PS3_jQuery/landing/script.js
@@ -1,28 +1,36 @@
-const NAVIGATION = {
-    products_link: '.banner',
-    about_us_link: '.services',
-    contact_us_link: '.forms'
-};
 
 const SPEED = 1000, PAGE_TOP = 0, PX_OFFSET = 100;
 
 $(document).ready(() => {
     const $upButton =  $('.up-button');
-    const $links = $('.products_link, .about_us_link, .contact_us_link');
+    const $links = $('#banner, #services, #forms');
+    const $webPage = $('html, body');
+    const $animated = $(':animated');
+    const stopScroll = () => {
+        $webPage.on("DOMMouseScroll mousewheel keyup touchmove scroll wheel",
+            () => { $webPage.stop();}
+        );
+    };
 
     $(window).scroll(() => {
-        ($(this).scrollTop() > PX_OFFSET) ? $upButton.fadeIn(SPEED) : $upButton.fadeOut(SPEED);
+        ($(this).scrollTop() > PX_OFFSET) ? $upButton.fadeIn(10) : $upButton.fadeOut(10);
     });
 
     $upButton.click(() => {
-        $('html, body').animate({scrollTop : PAGE_TOP}, SPEED);
+        if (!$animated.length) {
+            stopScroll();
+            $webPage.animate({scrollTop : PAGE_TOP}, SPEED);
+        }
     });
 
     $links.click(() => {
-        const $aim = $(`${NAVIGATION[event.toElement.className]}`);
-        const recenter = (window, aim, offset) => (window > aim) ?
-            offset - ((window - aim) / 2) : offset;
-        const altitude = recenter($(window).height(), $aim.height(), $aim[0].offsetTop );
-        $('html').animate({scrollTop: altitude}, SPEED);
+        if (!$animated.length) {
+            stopScroll();
+            const $aim = $('.' + `${event.target.id}`);
+            const recenter = (window, aim, offset) => (window > aim) ?
+                offset - ((window - aim) / 2) : offset;
+            const altitude = recenter($(window).height(), $aim.height(), $aim[0].offsetTop );
+            $webPage.animate({scrollTop: altitude}, SPEED);
+        }
     });
 });
