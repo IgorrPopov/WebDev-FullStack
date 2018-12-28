c8f610116b773dc383d9dd6ad430acec4b09a1b4
IgorPopov <ginger_tulips@protonmail.com>
2018-12-28 12:33:50 +0200

    removed one check, replaced maps with objects

--- WD_ST1_ATM/atm-template.js
+++ WD_ST1_ATM/atm-template.js
@@ -1,34 +1,33 @@
 /* witch function used by the user */
-const ACTIONS = new Map([
-    ['auth', 'authentication'],
-    ['check', 'check balance'],
-    ['getCash', 'receiving money'],
-    ['loadCash', 'recharge user account'],
-    ['load_cash', 'ATM cash replenishment'],
-    ['getReport', 'session report'],
-    ['logout', 'logout']
-]);
+const ACTIONS = {
+    auth: 'authentication',
+    check: 'check balance',
+    getCash: 'receiving money',
+    loadCash: 'recharge user account',
+    load_cash: 'ATM cash replenishment',
+    getReport: 'session report',
+    logout: 'logout'
+};
 /* all messages to user and to reports */
-const INFO = new Map([
-    ['isAuth', ['To do this action, you need to authorize', 'action of an unauthorized user']],
-    ['isLogged', ['You already logged in', 'multiple attempt to log in']],
-    ['isNotAdmin', ['This action isn\'t allowed for admin', 'attempt get cash by the admin user']],
-    ['isValidAmount', ['You entered invalid number', 'invalid input']],
-    ['isEnoughCashATM', ['ATM doesn\'t have enough money', 'not enough money in the ATM']],
-    ['isEnoughCash', ['You don\'t have enough money', 'user doesn\'t have enough money']],
-    ['isNotUser', ['This action isn\'t allowed for user', 'attempt to load cash to AMT by not an admin user']],
-    ['isUserInputString', ['Username and password must be Strings', 'not String input']],
-    ['isNumberString', ['The number must be passed as a String', 'not String input']],
-    ['userInput', ['Your input is invalid', 'invalid input during logging']],
-    ['verification', ['You entered an incorrect password or username', 'entered wrong password or login']],
-    ['logged in', ['You logged in', 'successful login']],
-    ['checked debet', ['Current debet: ', 'current debet ']],
-    ['Get your money', ['Get your money! Balance: ', 'cash withdrawal, balance: ']],
-    ['replenished balance', ['Account replenished, balance: ', 'account replenished, balance: ']],
-    ['replenished ATM balance', ['AMT cash balance increased to: ', 'AMT cash balance increased to: ']],
-    ['get report', ['___________________________________', 'got report']],
-    ['logged out', ['Logged out', 'logged out']]
-]);
+const INFO = {
+    isAuth: ['To do this action, you need to authorize', 'action of an unauthorized user'],
+    isLogged: ['You already logged in', 'multiple attempt to log in'],
+    isNotAdmin: ['This action isn\'t allowed for admin', 'attempt get cash by the admin user'],
+    isValidAmount: ['You entered invalid number', 'invalid input'],
+    isEnoughCashATM: ['ATM doesn\'t have enough money', 'not enough money in the ATM'],
+    isEnoughCash: ['You don\'t have enough money', 'user doesn\'t have enough money'],
+    isNotUser: ['This action isn\'t allowed for user', 'attempt to load cash to AMT by not an admin user'],
+    isUserInputString: ['Username and password must be Strings', 'not String input'],
+    userInput: ['Your input is invalid', 'invalid input during logging'],
+    verification: ['You entered an incorrect password or username', 'entered wrong password or login'],
+    logged_in: ['You logged in', 'successful login'],
+    checked_debet: ['Current debet: ', 'current debet '],
+    get_your_money: ['Get your money! Balance: ', 'cash withdrawal, balance: '],
+    replenished_balance: ['Account replenished, balance: ', 'account replenished, balance: '],
+    replenished_atm_balance: ['AMT cash balance increased to: ', 'AMT cash balance increased to: '],
+    get_report: ['___________________________________', 'got report'],
+    logged_out: ['Logged out', 'logged out']
+};
 /* colors of messages */
 const ERROR_COLOR = 'red', VALID_COLOR = 'green', REPORT_COLOR = 'blue';
 /* name of the error (name of the function witch identify an error) */
@@ -52,12 +51,12 @@ const ATM = {
     auth: function(number, pin) {
         this.addReport(checks(['isLogged', 'isUserInputString',
             'userInput', 'verification'], [number, pin, this]) ?
-            log('auth', 'logged in') : log('auth', current_error));
+            log('auth', 'logged_in') : log('auth', current_error));
     },
     // check current debet
     check: function() {
         this.addReport(checks(['isAuth'], [this]) ?
-            log('check', 'checked debet', this.current_user.debet) :
+            log('check', 'checked_debet', this.current_user.debet) :
             log('check', current_error));
     },
     // get cash - available for user only
@@ -65,9 +64,9 @@ const ATM = {
         const gettingCash = (amount) => {
             this.cash -= Number(amount);
             this.current_user.debet -= Number(amount);
-            return log('getCash', 'Get your money', this.current_user.debet);
+            return log('getCash', 'get_your_money', this.current_user.debet);
         };
-        this.addReport(checks(['isAuth', 'isNotAdmin', 'isNumberString', 'isValidAmount',
+        this.addReport(checks(['isAuth', 'isNotAdmin', 'isValidAmount',
             'isEnoughCashATM', 'isEnoughCash'], [this, amount]) ?
             gettingCash(amount) : log('getCash', current_error));
     },
@@ -76,32 +75,32 @@ const ATM = {
         const loadAccount = (amount) => {
             this.cash += Number(amount);
             this.current_user.debet += Number(amount);
-            return log('loadCash', 'replenished balance', this.current_user.debet);
+            return log('loadCash', 'replenished_balance', this.current_user.debet);
         };
-        this.addReport(checks(['isAuth', 'isNotAdmin', 'isNumberString', 'isValidAmount'],
+        this.addReport(checks(['isAuth', 'isNotAdmin', 'isValidAmount'],
             [this, amount]) ? loadAccount(amount) : log('loadCash', current_error));
     },
     // load cash to ATM - available for admin only - EXTENDED
     load_cash: function(addition) {
         const loadToATM = (addition) => {
             this.cash += Number(addition);
-            return log('load_cash', 'replenished ATM balance', this.cash);
+            return log('load_cash', 'replenished_atm_balance', this.cash);
         };
-        this.addReport(checks(['isAuth', 'isNotUser', 'isNumberString', 'isValidAmount'],
+        this.addReport(checks(['isAuth', 'isNotUser', 'isValidAmount'],
             [this, addition]) ? loadToATM(addition) : log('load_cash', current_error));
     },
-    // get report about cash actions - available for admin only - EXTENDED
+    // get_report about cash actions - available for admin only - EXTENDED
     getReport: function() {
         const reports = () => this.reports.forEach(
             (element, i) => log('', `${++i}) ${element.getReport()}\n`, REPORT_COLOR));
         this.addReport(checks(['isAuth', 'isNotUser'], [this]) ? (reports(),
-                log('getReport', 'get report')) :
+            log('getReport', 'get_report')) :
             log('getReport', current_error));
     },
     // log out
     logout: function() {
         if(checks(['isAuth'], [this])) {
-            this.addReport(log('logout', 'logged out'));
+            this.addReport(log('logout', 'logged_out'));
             this.current_user = false;
             this.current_type = false;
             this.is_auth = false;
@@ -143,8 +142,6 @@ function isEnoughCash(input) { return input[1] <= input[0].current_user.debet; }
 
 function isValidAmount(input) { return (/^[1-9]([0-9]*)$/.test(input[1]) && input[1] > 0); }
 
-function isNumberString(input) { return typeof input[1] === 'string';}
-
 function isUserInputString(input) {
     return typeof input[0] === 'string' && typeof input[1] === 'string';
 }
@@ -170,11 +167,11 @@ function log(action, massage, debet = -1) {
     if(action === ''){ // reports
         console.log(`%c${massage}`, `color: ${REPORT_COLOR}`);
     } else if (debet === -1) { // without numbers
-        console.log(`%c${INFO.get(massage)[0]}`, `color: ${color}`);
-        return ACTIONS.get(action) + ': ' + INFO.get(massage)[1];
+        console.log(`%c${INFO[massage][0]}`, `color: ${color}`);
+        return ACTIONS[action] + ': ' + INFO[massage][1];
     } else { // with numbers
-        console.log(`%c${INFO.get(massage)[0]}${debet}`, `color: ${color}`);
-        return ACTIONS.get(action) + ': ' + INFO.get(massage)[1] + debet;
+        console.log(`%c${INFO[massage][0]}${debet}`, `color: ${color}`);
+        return ACTIONS[action] + ': ' + INFO[massage][1] + debet;
     }
 }
 
